7e25943f71ab0ad8b0a1af2cedb1fd8165189dc9
erikduhem <erikduhem@192.168.4.147>
2016-06-24 11:53:58 +0200

    Commit de la classe pion et edition de Test

---
+++ gitPr/src/Entities/Pion.java
@@ -0,0 +1,12 @@
+package Entities;
+
+public class Pion extends Piece{
+
+	public Pion(String couleur, String position) {
+		super(couleur, position);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
