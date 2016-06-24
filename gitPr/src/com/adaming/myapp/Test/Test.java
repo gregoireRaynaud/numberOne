fc30f2141f14c90a735499e50fc2dea62a2bbb19
inti0335 <inti0335@INTI-0335>
2016-06-24 10:48:23 +0200

    Deuxième commit

---
+++ gitPr/src/com/adaming/myapp/Test/Test.java
@@ -0,0 +1,11 @@
+package com.adaming.myapp.Test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bonjour");
+
+	}
+
+}
